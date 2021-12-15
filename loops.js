c57020a58325fd6493f7cbbc7f258638f808b014
Mounika <bojjamounika8@gmail.com>
2021-12-15 23:54:41 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,27 @@
+var a=6;
+for(let i=1; i<=a; i++){
+console.log("Good Morning EveryOne")
+}
+
+
+
+var a=7;
+for(let i=1; i<=a; i++){
+console.log(i)
+}
+
+
+
+var sum=0;
+var n=50;
+for (let i=1; i<=n; i++){
+    sum +=i;
+}
+console.log(sum)
+
+let i=1;
+n=8;
+while(i<=n) {
+    console.log(i)
+    i+=1;
+}
